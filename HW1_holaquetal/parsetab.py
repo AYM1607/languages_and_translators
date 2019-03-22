5be6fab3b4b4cb8c2ab5c00fc3f65777c596e357
AYM1607 <u.g.a.mariano@gmail.com>
2019-03-21 18:38:52 -0600

    Added all token names

--- HW1_holaquetal/parsetab.py
+++ HW1_holaquetal/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'COMA HOLA QUE TAL\n    phrase : a QUE TAL\n    \n    a : HOLA COMA a\n      | HOLA\n    '
+_lr_signature = 'COMA HOLA QUE TAL\n    phrase : a QUE TAL\n    \n    a : HOLA COMA a\n      | HOLA\n      |\n    '
     
-_lr_action_items = {'HOLA':([0,5,],[3,3,]),'$end':([1,6,],[0,-1,]),'QUE':([2,3,7,],[4,-3,-2,]),'COMA':([3,],[5,]),'TAL':([4,],[6,]),}
+_lr_action_items = {'HOLA':([0,5,],[3,3,]),'QUE':([0,2,3,5,7,],[-4,4,-3,-4,-2,]),'$end':([1,6,],[0,-1,]),'COMA':([3,],[5,]),'TAL':([4,],[6,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -28,6 +28,7 @@ del _lr_goto_items
 _lr_productions = [
   ("S' -> phrase","S'",1,None,None,None),
   ('phrase -> a QUE TAL','phrase',3,'p_phrase','hola.py',28),
-  ('a -> HOLA COMA a','a',3,'p_a','hola.py',34),
-  ('a -> HOLA','a',1,'p_a','hola.py',35),
+  ('a -> HOLA COMA a','a',3,'p_a','hola.py',35),
+  ('a -> HOLA','a',1,'p_a','hola.py',36),
+  ('a -> <empty>','a',0,'p_a','hola.py',37),
 ]
